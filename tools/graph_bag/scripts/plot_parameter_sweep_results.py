9b1c6b804f8be8631b0f0143b65ad77c18524d57
Ryan Soussan <ryan.soussan@gmail.com>
2021-02-26 15:44:19 -0800

    added imu bias tester to param sweep results

--- tools/graph_bag/scripts/plot_parameter_sweep_results.py
+++ tools/graph_bag/scripts/plot_parameter_sweep_results.py
@@ -85,6 +85,9 @@ def create_plots(output_file, csv_file, value_combos_file):
     create_plot(pdf, csv_file, value_combos_file, 'imu_augmented_')
     create_plot(pdf, csv_file, value_combos_file, 'imu_augmented_orientation_')
     create_plot(pdf, csv_file, value_combos_file, 'imu_augmented_integrated_')
+    # IMU Bias Tester RMSEs
+    create_plot(pdf, csv_file, value_combos_file, 'imu_bias_tester_')
+    create_plot(pdf, csv_file, value_combos_file, 'imu_bias_tester_orientation_')
 
 
 if __name__ == '__main__':
