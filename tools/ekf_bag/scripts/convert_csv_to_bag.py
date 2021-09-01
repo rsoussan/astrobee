165b2db4ccc16f39833591bd3dfba49fdc233d8d
Ryan Soussan <ryan.soussan@gmail.com>
2021-09-01 11:01:52 -0700

    added saving imu and vl data from txt file

--- tools/ekf_bag/scripts/convert_csv_to_bag.py
+++ tools/ekf_bag/scripts/convert_csv_to_bag.py
@@ -20,6 +20,7 @@
 #import environment
 import rosbag
 import rospy
+from sensor_msgs.msg import Imu
 from geometry_msgs.msg import PoseStamped 
 from ff_msgs.msg import GraphState
 
@@ -40,6 +41,7 @@ class EkfLog(object):
     self.ekf = {'t': [], 'x': [], 'y': [], 'z': [], 'angle1': [], 'angle2': [], 'angle3': [], \
             'vx': [], 'vy': [], 'vz': [], 'ox': [], 'oy': [], 'oz': [], 'ax': [], 'ay': [], 'az': [], \
             'abx': [], 'aby': [], 'abz': [], 'gbx': [], 'gby': [], 'gbz': []}
+    self.vl = {'t': [], 'x': [], 'y': [], 'z': [], 'angle1': [], 'angle2': [], 'angle3': []}
     f = open(filename, 'r')
     for l in f:
       p = l.split(' ')
@@ -70,6 +72,16 @@ class EkfLog(object):
         self.ekf['gbx'].append(float(p[20]) * 180 / math.pi)
         self.ekf['gby'].append(float(p[21]) * 180 / math.pi)
         self.ekf['gbz'].append(float(p[22]) * 180 / math.pi)
+
+      elif l.startswith('VL ') and (len(p) - 9) / 5 >= 1:
+        if float(p[1]) > -1.0:
+          self.vl['t'].append(float(p[1]))
+          self.vl['x'].append(float(p[3]))
+          self.vl['y'].append(float(p[4]))
+          self.vl['z'].append(float(p[5]))
+          self.vl['angle1'].append(float(p[6]) * 180 / math.pi)
+          self.vl['angle2'].append(float(p[7]) * 180 / math.pi)
+          self.vl['angle3'].append(float(p[8]) * 180 / math.pi)
 
 
   def save_poses(self, bag):
@@ -109,31 +121,53 @@ class EkfLog(object):
       ekf_msg.gyro_bias.y = self.ekf['gby'][i]
       ekf_msg.gyro_bias.z = self.ekf['gbz'][i]
       bag.write('ekf_ekf_msg', ekf_msg, ekf_msg.header.stamp)
+      print("ekf t: " + str(self.ekf['t'][i]))
 
-def save_imu_data(input_bag, output_bag):
-  topics = ['/hw/imu']
-  for topic, msg, t in input_bag.read_messages(topics):
-    output_bag.write('/hw/imu', msg, t) 
+  def save_vl_poses(self, bag):
+    print(len(self.vl['t']))
+    for i in range(len(self.vl['t'])):
+      msg = PoseStamped() 
+      msg.pose.position.x = self.vl['x'][i]
+      msg.pose.position.y = self.vl['y'][i]
+      msg.pose.position.z = self.vl['z'][i]
+      euler_angles = [self.vl['angle1'][i], self.vl['angle2'][i], self.vl['angle3'][i]]
+      quaternion = scipy.spatial.transform.Rotation.from_euler('ZYX', euler_angles, degrees=True).as_quat()
+      msg.pose.orientation.x = quaternion[0] 
+      msg.pose.orientation.y = quaternion[1]
+      msg.pose.orientation.z = quaternion[2]
+      msg.pose.orientation.w = quaternion[3]
+      msg.header.stamp = rospy.Time.from_sec(self.vl['t'][i])
+      bag.write('/sparse_mapping/pose', msg, msg.header.stamp)
+
+
+  def save_imu_data(self, bag):
+    print(len(self.ekf['t']))
+    for i in range(len(self.ekf['t'])):
+      msg = Imu() 
+      msg.linear_acceleration.x = self.ekf['ax'][i]
+      msg.linear_acceleration.y = self.ekf['ay'][i]
+      msg.linear_acceleration.z = self.ekf['az'][i]
+      msg.angular_velocity.x = self.ekf['ox'][i]
+      msg.angular_velocity.y = self.ekf['oy'][i]
+      msg.angular_velocity.z = self.ekf['oz'][i]
+      msg.header.stamp = rospy.Time.from_sec(self.ekf['t'][i])
+      bag.write('/hw/imu', msg, msg.header.stamp)
 
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument('bagfile')
   parser.add_argument('txtfile')
   args = parser.parse_args()
-  if not os.path.isfile(args.bagfile):
-    print('Bag file ' + args.bagfile + ' does not exist.')
-    sys.exit()
 
   if not os.path.isfile(args.txtfile):
     print('txtfile ' + args.txtfile + ' does not exist.')
     sys.exit()
 
-  input_bag = rosbag.Bag(args.bagfile, 'r')
   output_bagfile = os.path.splitext(args.txtfile)[0] + '_results.bag'
   output_bag = rosbag.Bag(output_bagfile, 'w')
 
   log = EkfLog(args.txtfile)
   log.save_poses(output_bag)
-  save_imu_data(input_bag, output_bag)
+  log.save_imu_data(output_bag)
+  log.save_vl_poses(output_bag)
   output_bag.close()
