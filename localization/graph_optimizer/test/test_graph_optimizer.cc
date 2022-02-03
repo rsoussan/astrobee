a0817a886f018caff2ece5d70f9182255e29d74c
Ryan Soussan <ryan.soussan@gmail.com>
2022-02-02 18:26:01 -0800

    added second factor to graph opt test

--- localization/graph_optimizer/test/test_graph_optimizer.cc
+++ localization/graph_optimizer/test/test_graph_optimizer.cc
@@ -29,23 +29,51 @@ namespace go = graph_optimizer;
 namespace lc = localization_common;
 
 using PosePrior = gtsam::PriorFactor<gtsam::Pose3>;
+using VelocityPrior = gtsam::PriorFactor<gtsam::Velocity3>;
 
-TEST(GraphOptimizerTester, AddFactor) {
+TEST(GraphOptimizerTester, AddFactors) {
   const auto nodes = std::make_shared<go::Nodes>();
   const auto params = go::DefaultGraphOptimizerParams();
   go::GraphOptimizer optimizer(params, nodes);
   EXPECT_EQ(optimizer.TotalNumFactors(), 0);
   EXPECT_EQ(optimizer.NumFactors<PosePrior>(), 0);
 
-  // Add prior
+  // Add prior 1
   gtsam::Pose3 pose_1;
-  const auto key_1 = nodes->Add(pose_1);
+  const auto noisy_pose_1 = lc::AddNoiseToPose(pose_1, 1, 1);
+  const auto key_1 = nodes->Add(noisy_pose_1);
   const gtsam::Vector6 pose_prior_noise_sigmas((gtsam::Vector(6) << 1.0, 1.0, 1.0, 1.0, 1.0, 1.0).finished());
   const auto pose_noise = gtsam::noiseModel::Diagonal::Sigmas(pose_prior_noise_sigmas);
-  PosePrior pose_factor(key_1, pose_1, pose_noise);
-  optimizer.AddFactor(pose_factor);
-  EXPECT_EQ(optimizer.TotalNumFactors(), 1);
-  EXPECT_EQ(optimizer.NumFactors<PosePrior>(), 1);
+  PosePrior pose_factor_1(key_1, pose_1, pose_noise);
+  optimizer.AddFactor(pose_factor_1);
+  {
+    EXPECT_EQ(optimizer.TotalNumFactors(), 1);
+    EXPECT_EQ(optimizer.NumFactors<PosePrior>(), 1);
+    EXPECT_EQ(optimizer.NumFactors<VelocityPrior>(), 0);
+    ASSERT_TRUE(optimizer.Optimize());
+    const auto optimized_pose_1 = nodes->Get<gtsam::Pose3>(key_1);
+    ASSERT_TRUE(optimized_pose_1 != boost::none);
+    EXPECT_MATRIX_NEAR(pose_1, (*optimized_pose_1), 1e-6);
+  }
+
+  // Add prior 2
+  gtsam::Pose3 pose_2;
+  const auto noisy_pose_2 = lc::AddNoiseToPose(pose_2, 1, 1);
+  const auto key_2 = nodes->Add(noisy_pose_2);
+  PosePrior pose_factor_2(key_2, pose_2, pose_noise);
+  optimizer.AddFactor(pose_factor_2);
+  {
+    EXPECT_EQ(optimizer.TotalNumFactors(), 2);
+    EXPECT_EQ(optimizer.NumFactors<PosePrior>(), 2);
+    EXPECT_EQ(optimizer.NumFactors<VelocityPrior>(), 0);
+    ASSERT_TRUE(optimizer.Optimize());
+    const auto optimized_pose_1 = nodes->Get<gtsam::Pose3>(key_1);
+    ASSERT_TRUE(optimized_pose_1 != boost::none);
+    EXPECT_MATRIX_NEAR(pose_1, (*optimized_pose_1), 1e-6);
+    const auto optimized_pose_2 = nodes->Get<gtsam::Pose3>(key_2);
+    ASSERT_TRUE(optimized_pose_2 != boost::none);
+    EXPECT_MATRIX_NEAR(pose_2, (*optimized_pose_2), 1e-6);
+  }
 }
 
 // Run all the tests that were declared with TEST()
