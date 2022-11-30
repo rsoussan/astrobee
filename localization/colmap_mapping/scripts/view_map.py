e3d9966c70fa95a77f0403ff4af5a7d3731781ca
Ryan Soussan <ryan.soussan@gmail.com>
2022-11-30 11:13:18 -0800

    added view_map script

---
+++ localization/colmap_mapping/scripts/view_map.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2017, United States Government, as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+#
+# All rights reserved.
+#
+# The Astrobee platform is licensed under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+"""
+View the sparse map using the colmap gui. 
+"""
+
+import argparse
+import os
+import subprocess
+import sys
+
+def read_value(value_name, config_file):
+    value = None
+    for config_file_line in config_file:
+        line_strings = config_file_line.split("=")
+        if len(line_strings) > 0 and line_strings[0] == value_name:
+            value = line_strings[1]
+
+    return value
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "mapping_project_file",
+        help="Mapping project file used to create the sparse map, typically ending in mapping.ini.",
+    )
+    args = parser.parse_args()
+
+    if not os.path.isfile(args.mapping_project_file):
+        print("Mapping project file " + args.mapping_project_file + " does not exist.")
+        sys.exit()
+    
+    project_file = open(args.mapping_project_file, "r")
+
+    import_path = read_value(project_file, "output_path")
+    if not import_path:
+        print("Failed to read import path.")
+        sys.exit()
+
+    database_path = read_value(project_file, "database_path")
+    if not database_path:
+        print("Failed to read database path.")
+        sys.exit()
+ 
+    image_path = read_value(project_file, "image_path")
+    if not image_path:
+        print("Failed to read image path.")
+        sys.exit()
+ 
+    command = 'colmap gui --import_path ' + import_path + ' --database_path ' + database_path + ' --image_path ' + image_path
+    subprocess.call(command, shell=True)
